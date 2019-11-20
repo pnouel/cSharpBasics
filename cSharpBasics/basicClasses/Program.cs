f3da25a295c538261d8e168d02eb306961272569
pnouel <petronouel@gmail.com>
2019-11-19 21:23:15 -0300

    Demo de sintaxis y uso Classes

--- cSharpBasics/basicClasses/Program.cs
+++ cSharpBasics/basicClasses/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using basicClasses.clases;
 
 namespace basicClasses
 {
+   
     class Program
     {
         static void Main(string[] args)
         {
+            Person john = new Person();
+            john.FirstName = "john";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculadora calculadore = new Calculadora();
+            var result = calculadore.Add(2, 3);
+            Console.WriteLine(result);
+            /* Recibe dos parametro, se accede al metodo a traves de la 
+             clase, recordar guardar resultado en una variable
+             */
+
+            
         }
     }
 }
---
+++ cSharpBasics/basicClasses/clases/Calculadora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicClasses.clases
+{
+    public class Calculadora
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
