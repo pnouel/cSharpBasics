18258268619e4480d855ee7b9b13978e123b0985
pnouel <petronouel@gmail.com>
2019-11-28 10:00:49 -0300

    Ejercicios Loop

--- cSharpBasics/2_IngresarNumero/Program.cs
+++ cSharpBasics/2_IngresarNumero/Program.cs
@@ -10,6 +10,38 @@ namespace _2_IngresarNumero
     {
         static void Main(string[] args)
         {
-        }
+             /// <summary>
+        /// Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. 
+        /// Display the result on the console.
+        /// </summary>
+        /// 
+       /* var contador = 0 ;
+        for (var i = 0; i <= 100; i++)
+			{
+                if (i%3 == 0)
+                   contador++;
+	            
+	}     
+                    Console.WriteLine(contador);                
+                    */
+          /// <summary>
+        /// Write a program and continuously ask the user to enter a number. The loop terminates when the user 
+        /// enters “ok". Calculate the sum of all the previously entered numbers and display it on the console.
+        /// </summary>
+        /// 
+            var contador = 0;
+            while (true)
+	{
+               Console.WriteLine("Ingrese un numero o para terminar ingrese \"ok\" ");
+               var input = Console.ReadLine();
+                if (input.ToLower()=="ok")
+                    break;
+                contador += Convert.ToInt32(input);
+
+	}
+             Console.WriteLine(contador);           
+	
+			}
+        
     }
 }
--- cSharpBasics/4_Factorial/Program.cs
+++ cSharpBasics/4_Factorial/Program.cs
@@ -10,6 +10,28 @@ namespace _4_Factorial
     {
         static void Main(string[] args)
         {
+
+             /// <summary>
+        /// Write a program which takes a single argument from the console, computes the factorial and prints the 
+        /// value on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 
+        /// and display it as 5! = 120.
+        /// </summary>
+        /// 
+        Console.WriteLine("Ingresar Dato");
+             var input =Convert.ToInt32(Console.ReadLine());
+             var factorial = 1;
+           
+            for (var i = 0; i <= input; i++)
+			{
+                factorial *= i;
+                
+			}
+             Console.WriteLine("{0} es el Factorial de {1}", factorial,input);
+            
+           
+
+           
+
         }
     }
 }
